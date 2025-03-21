9c2545aebc3c3a685360889c3e4303bc4a2307b4
Sala G19 <CSalaG19@uniboyaca.edu.co>
2025-03-20 20:29:44 -0500

    se corrigienron algunos problemas del codigo

--- eliminar.php
+++ eliminar.php
@@ -3,11 +3,11 @@ require 'conexion.php';
 
 $id = $_GET['id'];
 
-// Eliminar la persona
+
 $stmt = $conn->prepare("DELETE FROM personas WHERE id = ?");
 $stmt->execute([$id]);
 
-// Redirigir a index.php
+
 header("Location: index.php");
 exit();
 ?>
